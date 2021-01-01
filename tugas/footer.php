546ceccf6720e169d10bc80b4aeecbc507ce817b
احمد ابو حسن <36786100+eby8zevin@users.noreply.github.com>
2021-01-01 09:36:27 +0700

    Create footer.php

---
+++ tugas/footer.php
@@ -0,0 +1,91 @@
+<footer class="footer">
+  <div class="container">
+    <div class="row">
+
+      <div class="col-sm-3">
+        <div class="footer-widget">
+          <h3>Contact Me</h3>
+          <div class="footer-widget-content">
+            <a href="mailto:ahmadabuhasan@mhs.stmik-yadika.ac.id" class="contact-link red"> click@myemail.com </a>
+            <a href="tel:+6282244103410" class="contact-link">Call Me!</a>
+            <div class="footer-social">
+              <ul>
+                <li><a href="https://github.com/eby8zevin" target="_blank"><i class="fa fa-github"></i></a></li>
+                <li><a href="https://kontakk.com/wa/2174bd" target="_blank"><i class="fa fa-whatsapp"></i></a></li>
+                <li><a href="http://stmik-yadika.ac.id" target="_blank"><i class="fa fa-globe"></i></a></li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+      
+      <div class="col-sm-3">
+        <div class="footer-widget">
+          <h3>My Project</h3>
+          <div class="footer-widget-content">
+            <div class="media">
+              <div class="media-left">
+                <a href="https://blackpink-marketplace.000webhostapp.com" target="_blank"><img class="media-object" src="/foto_produk/blackpink.jpeg" style="width:60px;height:60px;" alt="Customer"></a>
+              </div>
+              <div class="media-body">
+                <p><a href="https://blackpink-marketplace.000webhostapp.com" target="_blank">Customer. </a></p>
+                <span>November 19, 2020 </span>
+              </div>
+            </div>
+            <div class="media">
+              <div class="media-left">
+                <a href="https://blackpink-marketplace.000webhostapp.com/admin" target="_blank"><img class="media-object" src="/admin/assets/img/admin.png" style="width:60px;height:60px;" alt="Admin"></a>
+              </div>
+              <div class="media-body">
+                <p><a href="https://blackpink-marketplace.000webhostapp.com/admin" target="_blank">Admin. </a></p>
+                <span>November 19, 2020 </span>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      
+      <div class="col-sm-3">
+        <div class="footer-widget">
+          <h3>My Activity</h3>
+          <div class="footer-widget-content">
+            <div class="open-time ">
+              <ul class="opening-time">
+                <li><span><i class="fa fa-check"></i></span>
+                  <p><strong>e :</strong> eat( );</p>
+                </li>
+                <li><span><i class="fa fa-check"></i></span>
+                  <p><strong>s :</strong> sleep( );</p>
+                </li>
+                <li><span><i class="fa fa-check"></i></span>
+                  <p><strong>c :</strong> code( );</p>
+                </li>
+                <li><span><i class="fa fa-times"></i></span>
+                  <p class="clock-time"><strong>&nbsp;r :</strong> repeat( );</p>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="col-sm-3">
+        <div class="footer-widget">
+          <h3>SCAN</h3>
+          <div class="footer-widget-content">
+            <div class="images-gellary">
+              <ul>
+                <li><img src="2174bd.png" style="width:150px; height:150px;" alt="QR Code" title="QR Code"></li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+      
+    </div>
+  </div>
+</footer>
+
+    <div class="copyright text-center" style="color: white">
+        Copyright &copy; 2020 by <span>Ahmad Abu Hasan</span>
+    </div>
