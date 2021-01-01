bb885bacf4e1cf17600e0b8fa707ce184974b4f3
احمد ابو حسن <36786100+eby8zevin@users.noreply.github.com>
2021-01-01 09:43:09 +0700

    Create pdokoneksi.php

---
+++ tugas/pdokoneksi.php
@@ -0,0 +1,19 @@
+<?php
+try
+{
+    // buat koneksi dengan database
+    $dbh = new PDO('mysql:host=localhost;dbname=dbname', "username", "password");
+
+    // set error mode
+    $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    // hapus koneksi
+    //$dbh = null;
+}
+catch(PDOException $e)
+{
+    // tampilkan pesan kesalahan jika koneksi gagal
+    print "Koneksi atau query bermasalah: " . $e->getMessage() . "<br/>";
+    die();
+}
+?>
