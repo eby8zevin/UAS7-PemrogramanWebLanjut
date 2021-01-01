d5423a45f00cc142a2fe02c42aad09bf6a03ba8b
احمد ابو حسن <36786100+eby8zevin@users.noreply.github.com>
2021-01-01 07:46:28 +0700

    Create api-view.php

---
+++ api-view.php
@@ -0,0 +1,15 @@
+<?php
+include 'koneksi.php';
+
+$query = mysqli_query($koneksi, "SELECT * FROM produk");
+$array_data = array();
+
+while ($row = mysqli_fetch_assoc($query)) {
+    $array_data [] = $row;
+}
+
+header('Content-type: text/javascript');
+$json = json_encode($array_data, JSON_PRETTY_PRINT);
+file_put_contents('blackpink.json', $json);
+echo $json;
+?>
