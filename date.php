b474351b1b4b3a722138d8d2def4d1e0c05f0fe3
احمد ابو حسن <36786100+eby8zevin@users.noreply.github.com>
2021-01-01 07:54:22 +0700

    Create date.php

---
+++ date.php
@@ -0,0 +1,44 @@
+<?php
+function sekarang($mode = 'sekarang', $hari = '1', $bulan = '1', $tahun = '1', $indo = 'Januari', $bln = '1', $hari_ini = 'senin', $sekarang = 'jam')
+{
+    $hari = date('d');
+    $bulan = date('m');
+    $tahun = date('Y');
+    $indo = array(
+        'Januari',
+        'Februari',
+        'Maret',
+        'April',
+        'Mei',
+        'Juni',
+        'Juli',
+        'Agustus',
+        'September',
+        'Oktober',
+        'November',
+        'Desember'
+    );
+    $bln = $indo[$bulan - 1];
+    $hari_ini = "$hari $bln $tahun";
+    $sekarang = date('H:i:s');
+    if ($mode == 'jam')
+    {
+        return $sekarang;
+    }
+    elseif ($mode == 'tgl')
+    {
+        return $hari;
+    }
+    elseif ($mode == 'bln')
+    {
+        return $bln;
+    }
+    elseif ($mode == 'thn')
+    {
+        return $tahun;
+    }
+    else
+    {
+        return $hari_ini;
+    }
+}
